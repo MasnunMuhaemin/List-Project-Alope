a38c31f3716932bd23ca7e6946a189e921274991
MasnunMuhaemin <108683368+MasnunMuhaemin@users.noreply.github.com>
2025-08-11 23:31:57 +0700

    init

---
+++ src/pages/Home.tsx
@@ -0,0 +1,9 @@
+const Home = () => {
+    return (
+        <div className="my-20">
+            <h1 className="font-extrabold bg-fuchsia-800">from Hello World to Hello Lord</h1>
+        </div>
+    )
+}
+
+export default Home
