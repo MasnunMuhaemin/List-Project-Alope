6942c3a40635c3c4830f7b6693b51750d49c798d
MasnunMuhaemin <108683368+MasnunMuhaemin@users.noreply.github.com>
2025-08-17 21:29:09 +0700

    Fix the layout on List Project

--- src/sections/ListProject.tsx
+++ src/sections/ListProject.tsx
@@ -2,47 +2,21 @@ import Card from "../components/Card/Card";
 
 const ListProject = () => {
   return (
-    <section className="py-12 relative overflow-hidden text-white">
-      {/* Biru - kiri */}
-      <img
-        src="svg/Vector-12.svg"
-        className="absolute top-[15px] left-0 w-[350px] sm:w-[500px] lg:w-[700px] xl:w-[800px] max-w-none pointer-events-none"
-        alt="Blue Decoration"
-        loading="lazy"
-      />
-
+    <section className="relative py-20 overflow-hidden text-white">
+      <div className="absolute inset-0 h-1/2 bg-gradient-to-br from-black to-[#5998F4]/40" />
+      <div className="absolute inset-x-0 bottom-0 h-1/2 bg-gradient-to-tr from-black to-[#5998F4]/40" />
       <div className="relative z-10 container mx-auto px-6 text-center">
-        {/* Judul */}
-        <h2 className="text-3xl font-bold font-poppins">
-          List Project Alope
-        </h2>
+        <h2 className="text-3xl font-bold font-poppins">List Project Alope</h2>
         <p className="mt-3 max-w-2xl mx-auto font-poppins">
           Proyek-proyek yang telah berhasil kami kerjakan untuk berbagai
           kebutuhan klien, dengan standar kualitas terbaik dan hasil yang
           memuaskan.
         </p>
 
-        {/* Card List */}
         <div className="mt-10">
           <Card />
         </div>
       </div>
-
-      {/* Kuning - kanan */}
-      <img
-        src="svg/Vector-11.svg"
-        className="absolute top-[200px] right-0 w-[350px] sm:w-[400px] md:w-[500px] lg:w-[600px] xl:w-[700px] max-w-none pointer-events-none"
-        alt="Yellow Decoration"
-        loading="lazy"
-      />
-
-      {/* Ungu - kiri bawah */}
-      <img
-        src="svg/Vector-13.svg"
-        className="absolute bottom-0 left-0 w-[350px] sm:w-[400px] md:w-[600px] lg:w-[700px] xl:w-[800px] max-w-none pointer-events-none"
-        alt="Purple Decoration"
-        loading="lazy"
-      />
     </section>
   );
 };
